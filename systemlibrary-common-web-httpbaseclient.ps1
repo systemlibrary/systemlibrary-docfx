c131c454cbeac93005297c68506ac61e1d192750
ceo <ceo@systemlibrary.com>
2021-10-11 19:04:50 +0200

    ListItems in summaries are now green with bullet points Smaller font size to 12px from 14 inside tables and arguments Removed unused files

--- systemlibrary-common-web-httpbaseclient.ps1
+++ systemlibrary-common-web-httpbaseclient.ps1
@@ -1,7 +1,8 @@
 # REQUIRED OPTIONS
 $csprojFileFullPath = "C:\syslib\systemlibrary-common-web-httpbaseclient\source\SystemLibrary.Common.Web.HttpBaseClient\SystemLibrary.Common.Web.HttpBaseClient.csproj"
 
-$outputFolderFullPath = "C:\syslib\systemlibrary-common-web-httpbaseclient\docs\" # set to $null if you want outputFolder to be within the project folder
+#$outputFolderFullPath = "C:\syslib\systemlibrary-common-web-httpbaseclient\docs\" # set to $null if you want outputFolder to be within the project folder
+$outputFolderFullPath = "C:\temp\Docs\"
 
 $docfxConsoleExeFullPath = "C:\syslib\Packages\docfx.console.2.58.0\tools\docfx.exe" # download docfx.console as nuget package to some project, any project, and reference the .exe here
 
@@ -12,10 +13,11 @@ $footerWebsiteUrl = "https://www.systemlibrary.com/"
 $footerSiteTitle = "System Library Common Web HttpBaseClient"
 
 
-$relativeHostingPath = "/systemlibrary-common-web-httpbaseclient/" # If local IIS, most likely $null, "" or "/" is sufficient
+#$relativeHostingPath = "/systemlibrary-common-web-httpbaseclient/" # If local IIS, most likely $null, "" or "/" is sufficient
                                                     # If using 'github pages' it should be the name of the repo with prefix and suffix /
                                                     # It is the path of where css/js is loaded from, from the "root" of your domain
                                                     # Simply checking network tab in chrome while browsing your site after pushing to repo, files included in ~/docs/ folder, will reveal what it should (not) be
+$relativeHostingPath = ""													
 
 # OPTIONAL OPTIONS
 $cleanUp = $true # delete temp files generated during building documentation files
