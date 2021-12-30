ec740793b6ea2ba274ae5c22654355838e7b015d
systemlibrary <ceo@systemlibrary.com>
2021-12-30 13:38:07 +0100

    Added docfx script file for Common.Net.Json and Common.Web.HttpBaseClient

--- systemlibrary-core-common-net-json.ps1
+++ systemlibrary-core-common-net-json.ps1
@@ -1,18 +1,18 @@
 # REQUIRED OPTIONS
-$csprojFileFullPath = "C:\syslib\systemlibrary-core-common-net\source\SystemLibrary.Common.Net\SystemLibrary.Common.Net.csproj"
+$csprojFileFullPath = "C:\syslib\systemlibrary-core-common-net-json\source\SystemLibrary.Common.Net.Json\SystemLibrary.Common.Net.Json.csproj"
 
-$outputFolderFullPath = "C:\syslib\systemlibrary-core-common-net\docs\" # set to $null if you want outputFolder to be within the project folder
+$outputFolderFullPath = "C:\syslib\systemlibrary-core-common-net-json\docs\" # set to $null if you want outputFolder to be within the project folder
 #$outputFolderFullPath = "C:\temp\Docs\"
 
 $docfxConsoleExeFullPath = "C:\syslib\Packages\docfx.console.2.58.0\tools\docfx.exe" # download docfx.console as nuget package to some project, any project, and reference the .exe here
 
-$siteTitle = "System Library Common .NET"
-$footerGithubUrl = "https://github.com/systemlibrary/systemlibrary-core-common-net"
-$footerNugetUrl = "https://www.nuget.org/packages/SystemLibrary.Common.Net/"
+$siteTitle = "System Library Common .Net JSON"
+$footerGithubUrl = "https://github.com/systemlibrary/systemlibrary-core-common-net-json"
+$footerNugetUrl = "https://www.nuget.org/packages/SystemLibrary.Common.Net.Json/"
 $footerWebsiteUrl = "https://www.systemlibrary.com/"
-$footerSiteTitle = "System Library Common .NET"
+$footerSiteTitle = "System Library Common Net JSON"
 
-$relativeHostingPath = "/systemlibrary-core-common-net/"
+$relativeHostingPath = "/systemlibrary-core-common-net-json/"
 #$relativeHostingPath = "" # If local IIS, most likely $null, "" or "/" is sufficient
 # If using 'github pages' it should be the name of the repo with prefix and suffix /
 # It is the path of where css/js is loaded from, from the "root" of your IIS site
--- systemlibrary-core-common-web-httpbaseclient.ps1
+++ systemlibrary-core-common-web-httpbaseclient.ps1
@@ -1,18 +1,18 @@
 # REQUIRED OPTIONS
-$csprojFileFullPath = "C:\syslib\systemlibrary-core-common-net\source\SystemLibrary.Common.Net\SystemLibrary.Common.Net.csproj"
+$csprojFileFullPath = "C:\syslib\systemlibrary-core-common-web-httpbaseclient\source\SystemLibrary.Common.Web.HttpBaseClient\SystemLibrary.Common.Web.HttpBaseClient.csproj"
 
-$outputFolderFullPath = "C:\syslib\systemlibrary-core-common-net\docs\" # set to $null if you want outputFolder to be within the project folder
+$outputFolderFullPath = "C:\syslib\systemlibrary-core-common-web-httpbaseclient\docs\" # set to $null if you want outputFolder to be within the project folder
 #$outputFolderFullPath = "C:\temp\Docs\"
 
 $docfxConsoleExeFullPath = "C:\syslib\Packages\docfx.console.2.58.0\tools\docfx.exe" # download docfx.console as nuget package to some project, any project, and reference the .exe here
 
-$siteTitle = "System Library Common .NET"
-$footerGithubUrl = "https://github.com/systemlibrary/systemlibrary-core-common-net"
-$footerNugetUrl = "https://www.nuget.org/packages/SystemLibrary.Common.Net/"
+$siteTitle = "System Library Common Web BaseHttpClient"
+$footerGithubUrl = "https://github.com/systemlibrary/systemlibrary-core-common-web-httpbaseclient"
+$footerNugetUrl = "https://www.nuget.org/packages/SystemLibrary.Common.Web.HttpBaseClient/"
 $footerWebsiteUrl = "https://www.systemlibrary.com/"
-$footerSiteTitle = "System Library Common .NET"
+$footerSiteTitle = "System Library Common Web HttpBaseClient"
 
-$relativeHostingPath = "/systemlibrary-core-common-net/"
+$relativeHostingPath = "/systemlibrary-core-common-web-httpbaseclient/"
 #$relativeHostingPath = "" # If local IIS, most likely $null, "" or "/" is sufficient
 # If using 'github pages' it should be the name of the repo with prefix and suffix /
 # It is the path of where css/js is loaded from, from the "root" of your IIS site
