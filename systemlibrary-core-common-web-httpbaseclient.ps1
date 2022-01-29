4f75ccfb532c74f9a788ce728f1325396278779f
systemlibrary <ceo@systemlibrary.com>
2022-01-29 02:39:32 +0100

    BaseHttpClient renamed to HttpBaseClient

--- systemlibrary-core-common-web-httpbaseclient.ps1
+++ systemlibrary-core-common-web-httpbaseclient.ps1
@@ -6,7 +6,7 @@ $outputFolderFullPath = "C:\syslib\systemlibrary-core-common-web-httpbaseclient\
 
 $docfxConsoleExeFullPath = "C:\syslib\Packages\docfx.console.2.58.0\tools\docfx.exe" # download docfx.console as nuget package to some project, any project, and reference the .exe here
 
-$siteTitle = "System Library Common Web BaseHttpClient"
+$siteTitle = "System Library Common Web HttpBaseClient"
 $footerGithubUrl = "https://github.com/systemlibrary/systemlibrary-core-common-web-httpbaseclient"
 $footerNugetUrl = "https://www.nuget.org/packages/SystemLibrary.Common.Web.HttpBaseClient/"
 $footerWebsiteUrl = "https://www.systemlibrary.com/"
