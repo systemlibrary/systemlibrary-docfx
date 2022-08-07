d7372f887cb91b3f648fd8264282a47b380200dd
systemlibrary <ceo@systemlibrary.com>
2022-08-07 14:57:26 +0200

    - Brown background is changed to light-black - green comments in summary1 are no longer prefixed with // - green comments in summary1 have font "text" instead of "code" - comments above methods have font "text" instead of "code"

--- systemlibrary-common-episerver.ps1
+++ systemlibrary-common-episerver.ps1
@@ -3,8 +3,8 @@ Set-Location $PSScriptRoot
 # REQUIRED OPTIONS
 $csprojFileFullPath = "C:\syslib\systemlibrary-common-episerver\source\SystemLibrary.Common.Episerver\SystemLibrary.Common.Episerver.csproj"
 
-$outputFolderFullPath = "C:\syslib\systemlibrary-common-episerver\docs\" # set to $null if you want outputFolder to be within the project folder
-#$outputFolderFullPath = "C:\temp\Docs\"
+#$outputFolderFullPath = "C:\syslib\systemlibrary-common-episerver\docs\" # set to $null if you want outputFolder to be within the project folder
+$outputFolderFullPath = "C:\temp\Docs\"
 
 $siteTitle = "System Library Common Episerver"
 $footerGithubUrl = "https://github.com/systemlibrary/systemlibrary-common-episerver/"
@@ -12,8 +12,8 @@ $footerNugetUrl = "https://www.nuget.org/packages/SystemLibrary.Common.Episerver
 $footerWebsiteUrl = "https://www.systemlibrary.com/"
 $footerSiteTitle = "System Library Common Episerver"
 
-$relativeHostingPath = "/systemlibrary-common-episerver/"
-#$relativeHostingPath = "" # If local IIS, most likely $null, "" or "/" is sufficient
+#$relativeHostingPath = "/systemlibrary-common-episerver/"
+$relativeHostingPath = "" # If local IIS, most likely $null, "" or "/" is sufficient
 # If using 'github pages' it should be the name of the repo with prefix and suffix /
 # It is the path of where css/js is loaded from, from the "root" of your IIS site
 # Simply checking network tab in chrome while browsing your site will reveal what it should be, as youll get 404/red for css/js requests
