da11d9a470dd06e71ee08ffc1482fe58c06c47e3
systemlibrary <ceo@systemlibrary.com>
2026-05-17 13:58:49 +0200

    Removed the "ctor codewrapper jljs", replaced with just the name

--- scripts/Powershell/11-color-public-enum.ps1
+++ scripts/Powershell/11-color-public-enum.ps1
@@ -1,18 +1,18 @@
-$colorPublicEnum = @(
-    @{ Key = 'public enum '; Value = "<span class='class-enum-keyword-enum'>enum </span>" }
-)
+# $colorPublicEnum = @(
+#     @{ Key = 'public enum '; Value = "<span class='class-enum-keyword-enum'>enum </span>" }
+# )
 
-foreach ($doc in $htmlFiles) {
-    if (-not $doc.Content) { 
-        continue
-    }
+# foreach ($doc in $htmlFiles) {
+#     if (-not $doc.Content) { 
+#         continue
+#     }
 
-    foreach ($kv in $colorPublicEnum) {
-        if ($doc.Content.Contains($kv.Key)) {
-            $doc.Content = $doc.Content.Replace($kv.Key, $kv.Value)
-            $doc.Changed = $true
-        }
-    }
-}
+#     foreach ($kv in $colorPublicEnum) {
+#         if ($doc.Content.Contains($kv.Key)) {
+#             $doc.Content = $doc.Content.Replace($kv.Key, $kv.Value)
+#             $doc.Changed = $true
+#         }
+#     }
+# }
 
-Out "Colored public enums"
+# Out "Colored public enums"
