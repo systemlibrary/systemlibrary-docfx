2ad05dff816b954f0c2674b3686d4ceeb2b6fb20
systemlibrary <ceo@systemlibrary.com>
2026-09-20 23:02:36 +0200

    Skip DocsApi and docsapi

--- scripts/Powershell/13-copy-site-to-output.ps1
+++ scripts/Powershell/13-copy-site-to-output.ps1
@@ -5,12 +5,14 @@ Start-Sleep -Milliseconds 500
 
 # docsapi files are generated and linked by docfx toc file, cannot be touched
 $skipDocsApiFiles = Join-Path $Output "docsapi"
+$skipDocsApiFiles2 = Join-Path $Output "DocsApi"
 
 
 # RENAME FILES AND FOLDERS INSIDE OUTPUT TO "<name>.LOWERCASE"
 Get-ChildItem -Path $Output -Recurse |
     Where-Object {
-        $_.FullName -notlike "$skipDocsApiFiles/*"
+        $_.FullName -notlike "$skipDocsApiFiles/*" -and 
+        $_.FullName -notlike "$skipDocsApiFiles2/*"
     } |
     Sort-Object FullName -Descending |
     ForEach-Object {
